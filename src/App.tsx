fa582faa43684707da961dc69627482b65371ae9
M. Bagher Abiat <zorofight94@gmail.com>
2020-08-02 17:53:10 +0430

    App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import React, { useState } from 'react';
-const App: React.FC = () => {
-	return <div>hello</div>;
-};
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Store } from './interfaces';
+import { Provider } from './store';
+import Background from './components/Background';
+import Home from './routes/Home';
 
+const App: React.FC = () => {
+	const [
+		state,
+		dispatch
+	] = React.useState<Store | undefined>(undefined);
+	return (
+		<React.Fragment>
+			<Background />
+			<Provider state={state} dispatch={dispatch}>
+				<Router>
+					<Route path='/' exact component={Home} />
+					<Route path='/game' exact />
+				</Router>
+			</Provider>
+		</React.Fragment>
+	);
+};
 export default App;
