707418928a07ad59b88fb1bd7e5a98bf74d07419
M. Bagher Abiat <zorofight94@gmail.com>
2020-08-08 11:07:53 +0430

    svg

--- src/assets/index.d.ts
+++ src/assets/index.d.ts
@@ -1,19 +1,14 @@
-declare module '*.svg' {
-	// import React = require('react');
-	// 	const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
-	// 	export default ReactComponent;
-	// const src: string;
-	// export default src;
-	const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-	export default content;
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
 }
 
-declare module '*.jpg' {
-	const content: string;
-	export default content;
+declare module "*.jpg" {
+  const content: string;
+  export default content;
 }
 
-declare module '*.png' {
-	const content: string;
-	export default content;
+declare module "*.png" {
+  const content: string;
+  export default content;
 }
