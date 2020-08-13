c875136fc00fc95cd349ba21608712d7496678c6
M. Bagher Abiat <zorofight94@gmail.com>
2020-08-14 00:40:13 +0430

    Responsive bug solved

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -20,48 +20,14 @@ const useStyles = createUseStyles({
     position: "relative",
     animation: "$animation 2s infinite ",
   },
-  svg: {
+  span: {
     position: "absolute",
     opacity: 0.6,
-    // animation: "$animation 6.5s infinite",
   },
 });
 const Background: React.FC = () => {
   const classes = useStyles();
-  const svgElements = [];
   const alphabetical = "ABCDEFGHIJKLMNORPSTUVWXYZ".split("");
-
-  //   for (let index = 0; index < 15; index++) {
-  //     const randomStyle = Math.random();
-  //     svgElements[svgElements.length] = (
-  //       <svg
-  //         key={index}
-  //         style={{
-  //           position: "relative",
-  //           top: `${Math.random() * window.innerHeight + 1}px`,
-  //           left: `${index * 9}rem`,
-  //           transform: `rotate(${Math.random() * 180 +
-  //             1}deg) matrix(-0.15, 0.99, 0.99, 0.15, 0, 0)`,
-  //           stroke: randomStyle > 0.5
-  //             ? "rgba(250, 210, 46, 1)"
-  //             : "rgba(0, 0, 0, 1)",
-  //           filter: `drop-shadow( 0px 5px 5px rgba(${
-  //             randomStyle > 0.5 ? "250, 210, 46, 0.75" : "0, 0, 0, 0.55"
-  //           }))`,
-  //         }}
-  //         xmlns="http://www.w3.org/2000/svg"
-  //         width="42"
-  //         height="48"
-  //         viewBox="0 0 39 48"
-  //         fill="none"
-  //       >
-  //         <path
-  //           d="M1.00903 7.60134C34.0727 0.601402 38.8627 44.2047 34.0727 41.5695"
-  //           strokeWidth="4"
-  //         />
-  //       </svg>
-  //     );
-  //   }
   return (
     <div className={classes.background}>
       {alphabetical.map((letter, index) => {
@@ -69,7 +35,7 @@ const Background: React.FC = () => {
 
         return (<span
           key={letter}
-          className={classes.svg}
+          className={classes.span}
           style={{
             position: "relative",
             top: `${constRandom * window.innerHeight / 18 + 1}rem`,
